2df45078733ab9de6869e0838d190a14cef9fa23
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2025-04-09 21:44:24 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:product_gallery/features/home/home_page.dart';
+
+void main() async {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
